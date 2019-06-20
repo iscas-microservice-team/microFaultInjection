80af85b56cc98a34459ddd91fd76dfecda2749e9
XinyaoTian <leontian1024@gmail.com>
2019-06-20 21:40:07 +0800

    Basic functions finished

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from app import app
 
 
+# ToDo: 在欢迎页面加入简易使用说明。
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+# ToDo: 制作简易的用户交互界面
 
--- config.py
+++ config.py
@@ -5,27 +5,3 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcdef020301abc8c86f'
-
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'app.db')
-
-    # my postgres in K8s
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
-    # K8s config
-    # POSTGRES_HOST = os.environ.get('COFFEESHOP_POSTGRES_SERVICE_HOST')
-    # POSTGRES_PORT = os.environ.get('COFFEESHOP_POSTGRES_SERVICE_PORT')
-    # POSTGRES_DBNAME = os.environ.get('POSTGRES_DBNAME')
-    # POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME')
-    # POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    # POSTGRES_PROTOCOL = os.environ.get('POSTGRES_PROTOCOL')
-    # POSTGRES_SSLMODE = os.environ.get('POSTGRES_SSLMODE')
-    #
-    # # assembled Database URI
-    # SQLALCHEMY_DATABASE_URI = POSTGRES_PROTOCOL + '://' + POSTGRES_USERNAME + ':' + POSTGRES_PASSWORD + '@' +\
-    #                           POSTGRES_HOST + ':' + POSTGRES_PORT + '/' + POSTGRES_DBNAME + '?sslmode=' + POSTGRES_SSLMODE
-
-    # protocol + `://` + dbUsername + `:` + dbPassword + `@` + dbIp + `:` + dbPort + `/` + dbName + `?sslmode=` +sslMode
-    # SQLALCHEMY_DATABASE_URI = 'postgres://dbuser:love_coffee@23.106.158.242:31432/coffeeshop?sslmode=disable'
-
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False

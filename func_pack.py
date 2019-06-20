2d0fefd9e5643ea953e49ae18904999b02003071
XinyaoTian <leontian1024@gmail.com>
2019-06-20 19:45:34 +0800

    Fault injection in func_pack

--- func_pack.py
+++ func_pack.py
@@ -16,51 +16,47 @@ def cal(num, dif, mode):
 
 
 # 将薛师兄代码里的数字转变为更加易懂的参数输入
-def fault_translator(error_type):
-
-
-
-def fault_injection():
-    print("fault_injection func")
-    env_dict = os.environ
-    # fault_list = {'cpu' : 1, 'mem' : 2, 'disk' : 3, 'net' : 4}
-    fault_type = env_dict.get('FAULT_TYPE') or 'cpu'
-    # cpu : FAULT_TYPE、THREAD_NUM、MODE
-    # mem : FAULT_TYPE、THREAD_NUM、MEM_SIZE、MODE
-    # disk : FAULT_TYPE、THREAD_NUM、MODE
-    # net : FAULT_TYPE、NET_PORT、MODE
+def fault_injection(fault_type, **kwargs):
+    logging.info("Ready to inject fault.")
+    # fault_type have 4 types: 'cpu', 'mem', 'disk', 'net'
     if fault_type == 'cpu':
         print("cpu error injection")
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        logging.info("cpu error injection success!")
-        thread_num = env_dict.get('THREAD_NUM') or '4'
-        duration = env_dict.get('DURATION') or '100'
-        # mode = env_dict.get('MODE') or '1'
-        print("stress -c %s -t %s" %(thread_num, duration))
-        os.system("stress -c %s -t %s" %(thread_num, duration))
+        # CPU Fault has 2 args: thread_num, duration
+        thread_num = kwargs['thread_num'] or '4'
+        duration = kwargs['duration'] or '100'
+        logging.info("stress -c %s -t %s" % (thread_num, duration))
+        os.system("stress -c %s -t %s" % (thread_num, duration))
+
     elif fault_type == 'mem':
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        logging.info("mem error injection success!")
-        thread_num = env_dict.get('THREAD_NUM') or '4'
-        mem_size = env_dict.get('MEM_SIZE') or '5M'
-        duration = env_dict.get('DURATION') or '100'
+        # Memory Fault has 3 args: thread_num, mem_size, duration
+        thread_num = kwargs['thread_num'] or '4'
+        mem_size = kwargs['mem_size'] or '5M'
+        duration = kwargs['duration'] or '100'
         # mode = env_dict('MODE') or '1'
-        os.system("stress --vm %s --vm-bytes %s --vm-keep -t %s" %(thread_num, mem_size, duration))
+        os.system("stress --vm %s --vm-bytes %s --vm-keep -t %s" % (thread_num, mem_size, duration))
+
     # iostat -x -k -d 1
     elif fault_type == 'disk':
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        logging.info("disk error injection success!")
-        io_times = env_dict.get("IO_TIMES") or '4'
-        duration = env_dict.get('DURATION') or '100'
+        io_times = kwargs['io_times'] or '4'
+        duration = kwargs['duration'] or '100'
         # mode = env_dict('MODE') or '1'
         os.system("stress -i %s -t %s" % (io_times, duration))
+
     elif fault_type == 'net':
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        logging.info("net error injection success!")
-        net_port = env_dict.get('NET_PORT')
-        if not env_dict.get('NET_FLAG'):
-            os.system("iperf3 -s -p %s" % (net_port))
-        os.system("export NET_FLAG=1")
+        net_port = kwargs['NET_PORT']
+        # if not env_dict.get('NET_FLAG'):
+        if kwargs['net_flag'] is None:
+            os.system("iperf3 -s -p %s" % net_port)
+
+    # 若错误类型不在这4种之内，则返回 None
+    else:
+        return None
+
+    pass
 
 
 if __name__ == '__main__':

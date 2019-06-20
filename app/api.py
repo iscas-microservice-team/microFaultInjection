1ca9c86bb33c10dc7314a1ac04dad74c28e430c3
XinyaoTian <leontian1024@gmail.com>
2019-06-20 20:39:47 +0800

    Fault-injection API

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import jsonify
-from flask import request, url_for, redirect
-from werkzeug.http import HTTP_STATUS_CODES
+from flask import request
+from func_pack import fault_injection
 
 
 # API-test GET Method
@@ -16,4 +16,20 @@ def test_get():
 # Fault-Injection Method
 @app.route('/fault-inject', method=['POST'])
 def fault_injection():
-    pass
+    fault_type = request.form.get('fault_type')
+    thread_num = request.form.get('thread_num')
+    duration = request.form.get('duration')
+    mem_size = request.form.get('mem_size')
+    io_times = request.form.get('io_times')
+    net_port = request.form.get('net_port')
+    net_flag = request.form.get('net_flag')
+    # deliver all the args from POST into fault_injection
+    result = fault_injection(fault_type, thread_num=thread_num, duration=duration, mem_size=mem_size,
+                             io_times=io_times, net_port=net_port, net_flag=net_flag)
+    if result is not None:
+        return jsonify(result)
+    else:
+        info = [
+            {'Info': 'Your faults are injecting. Please wait.'}
+        ]
+        return jsonify(info)
--- func_pack.py
+++ func_pack.py
@@ -12,42 +12,74 @@ def fault_injection(fault_type, **kwargs):
         print("cpu error injection")
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         # CPU Fault has 2 args: thread_num, duration
-        thread_num = kwargs['thread_num'] or '4'
-        duration = kwargs['duration'] or '100'
+        if 'thread_num' not in kwargs:
+            thread_num = '4'
+        else:
+            thread_num = kwargs['thread_num']
+        if 'duration' not in kwargs:
+            duration = '100'
+        else:
+            duration = kwargs['duration']
+        # using linux command to inject faults.
         logging.info("stress -c %s -t %s" % (thread_num, duration))
         os.system("stress -c %s -t %s" % (thread_num, duration))
+        return None
 
     elif fault_type == 'mem':
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         # Memory Fault has 3 args: thread_num, mem_size, duration
-        thread_num = kwargs['thread_num'] or '4'
-        mem_size = kwargs['mem_size'] or '5M'
-        duration = kwargs['duration'] or '100'
-        # mode = env_dict('MODE') or '1'
+        if 'thread_num' not in kwargs:
+            thread_num = '4'
+        else:
+            thread_num = kwargs['thread_num']
+        if 'mem_size' not in kwargs:
+            mem_size = '5M'
+        else:
+            mem_size = kwargs['mem_size']
+        if 'duration' not in kwargs:
+            duration = '100'
+        else:
+            duration = kwargs['duration']
+
         os.system("stress --vm %s --vm-bytes %s --vm-keep -t %s" % (thread_num, mem_size, duration))
+        return None
 
     # iostat -x -k -d 1
     elif fault_type == 'disk':
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        io_times = kwargs['io_times'] or '4'
-        duration = kwargs['duration'] or '100'
-        # mode = env_dict('MODE') or '1'
+        # args
+        if 'io_times' not in kwargs:
+            io_times = '4'
+        else:
+            io_times = kwargs['io_times']
+        if 'duration' not in kwargs:
+            duration = '100'
+        else:
+            duration = kwargs['duration']
+
         os.system("stress -i %s -t %s" % (io_times, duration))
+        return None
 
     elif fault_type == 'net':
         logging.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-        net_port = kwargs['NET_PORT']
-        # if not env_dict.get('NET_FLAG'):
-        if kwargs['net_flag'] is None:
-            os.system("iperf3 -s -p %s" % net_port)
+        if 'net_port' not in kwargs:
+            net_port = '100'
+        else:
+            net_port = kwargs['net_port']
+
+        os.system("iperf3 -s -p %s" % net_port)
+        return None
 
     # 若错误类型不在这4种之内，则返回 None
     else:
-        return None
+        error = [
+            {'error': 'fault_type must in the range of cpu, mem, dish and net. Your fault_type is %s.' % fault_type}
+        ]
+        return error
 
     pass
 
 
 if __name__ == '__main__':
-    fault_injection('cpu', thread_num=4, duration=100)
+    fault_injection('cpu')
 
